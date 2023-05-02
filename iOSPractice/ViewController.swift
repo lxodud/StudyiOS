9303c10388fba6ee6bf451ce852a2572fd409e41
lxodud <xodud1998223@naver.com>
2023-05-02 23:13:06 +0900

    initial commit

---
+++ iOSPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSPractice
+//
+//  Created by 이태영 on 2023/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
