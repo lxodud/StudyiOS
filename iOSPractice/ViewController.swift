505ee816681969ef856a9d8fe202eb1ce1d091be
lxodud <xodud1998223@naver.com>
2023-07-07 23:33:11 +0900

    feat: type erasure 구현

--- iOSPractice/ViewController.swift
+++ iOSPractice/ViewController.swift
@@ -8,25 +8,73 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    let someView = CustomView()
+    let label = Label()
+    let button = Button()
+    let textField = TextField()
+    
+    var component: [AnyComponent] = []
     
     override func viewDidLoad() {
-        configureLayout()
+        component.append(AnyComponent(label))
+        component.append(AnyComponent(button))
+        component.append(AnyComponent(textField))
     }
 }
 
-extension ViewController {
-    private func configureLayout() {
-        view.addSubview(someView)
-        someView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let safeArea = view.safeAreaLayoutGuide
-        
-        NSLayoutConstraint.activate([
-            someView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-            someView.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor),
-            someView.widthAnchor.constraint(equalTo: safeArea.widthAnchor, multiplier: 0.5),
-            someView.heightAnchor.constraint(equalTo: safeArea.heightAnchor, multiplier: 0.2),
-        ])
+protocol Componentable {
+    associatedtype Content
+    
+    func getContentView() -> Content
+}
+
+struct Label: Componentable {
+    typealias Content = UILabel
+    
+    func getContentView() -> UILabel {
+        return UILabel()
+    }
+}
+
+struct Button: Componentable {
+    typealias Content = UIButton
+    
+    func getContentView() -> UIButton {
+        return UIButton()
+    }
+}
+
+struct TextField: Componentable {
+    typealias Content = UITextField
+    
+    func getContentView() -> UITextField {
+        return UITextField()
+    }
+}
+
+protocol AnyComponentBase {
+    func getContentView() -> Any
+}
+
+struct AnyComponentBox<ConcreteComponent: Componentable>: AnyComponentBase {
+    var concrete: ConcreteComponent
+    
+    init(concrete: ConcreteComponent) {
+        self.concrete = concrete
+    }
+    
+    func getContentView() -> Any {
+        return concrete.getContentView()
+    }
+}
+
+struct AnyComponent: Componentable {
+    private let box: AnyComponentBase
+    
+    init<Component: Componentable>(_ component: Component) {
+        box = AnyComponentBox(concrete: component)
+    }
+    
+    func getContentView() -> Any {
+        return box.getContentView()
     }
 }
